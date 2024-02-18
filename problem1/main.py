bbf37ffb2845d254afc709ac35093e106fddfc88
alisofyansaputra <Buffware4@gmail.com>
2024-02-18 23:16:06 +0700

    nama.problem1

--- problem1/main.py
+++ problem1/main.py
@@ -1,4 +1,9 @@
-name = input("Ali Sofyan Saputra :")
+# input
+nama = "Kobar"
+
+
+# kode disini
+name = input("Inputkan nama: ")
 
 # Membuat output sesuai dengan format yang diinginkan
 output = f"Hello {name}! Saya Golang, bahasa yang sangat menyenangkan."
