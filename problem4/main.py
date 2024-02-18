69ef78ff222fbfeb2d20e37f051457f68eb27d9d
alisofyansaputra <Buffware4@gmail.com>
2024-02-19 00:03:27 +0700

    Harga.problem4

--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,9 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+
+harga_awal = float(input("Masukkan harga awal (Rp): "))
+diskon = float(input("Masukkan diskon (%): "))
+
+# Menghitung harga diskon
+harga_diskon
