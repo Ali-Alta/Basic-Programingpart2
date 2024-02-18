5dd412a7ab70ab5a0ebe40da4d29341745b3077c
alisofyansaputra <Buffware4@gmail.com>
2024-02-18 23:25:46 +0700

    segitiga.problem2

--- problem2/main.py
+++ problem2/main.py
@@ -4,3 +4,12 @@ tinggi = 25.0
 
 
 # kode disini
+# Input alas dan tinggi dari pengguna
+alas = float(input("alas (20 cm): "))
+tinggi = float(input("tinggi (25.0 cm): "))
+
+# Menghitung luas segitiga
+luas_segitiga = 0.5 * alas * tinggi
+
+# Menampilkan hasil
+print(f"Luas segitiga dengan alas {alas} cm dan tinggi {tinggi} cm adalah {luas_segitiga} cm².")
